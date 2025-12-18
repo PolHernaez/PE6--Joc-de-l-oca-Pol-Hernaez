7f69abe39757e903ea265a627347f49d5cff9b21
PolHernáez <alu.pol.hernaez@mataro.epiaedu.cat>
2025-12-18 17:17:07 +0100

    refactor game state management and player actions

--- JocOca.java
+++ JocOca.java
@@ -8,10 +8,12 @@ public class JocOca {
     }
 
     Scanner h = new Scanner(System.in);
+    boolean[] sanció;
     String[] noms;
     int numPlayers;
     boolean seguentTorn = true;
     boolean end = false;
+    int[] rondesSanció;
 
     public void principal() {
         System.out.println("EL JOC DE L'OCA \n");
@@ -84,11 +86,12 @@ public class JocOca {
 
     public void definirOrdre(int num, String[] names) {
         int[] tirades = new int[num];
+        rondesSanció = new int[num];
 
         System.out.println("\n--- TIRADES PER DEFINIR L'ORDRE ---");
 
         for (int c = 0; c < num; c++) {
-            tirades[c] = (int) (Math.random() * 12) + 1;
+            tirades[c] = (int) (Math.random() * 6) + 1;
             System.out.println("El jugador " + names[c] + " ha tret un " + tirades[c]);
         }
         for (int i = 0; i < num - 1; i++) {
@@ -113,37 +116,63 @@ public class JocOca {
 
     public void play() {
         int[] tauler = new int[numPlayers];
-
+        sanció = new boolean[numPlayers];
         System.out.println("\nCOMENÇA LA PARTIDA");
-        int torn = 0;
-
+        int torn = 5;
+        int ronda = 0;
         do {
-    seguentTorn = false;
+
+            seguentTorn = false;
+
             while (!seguentTorn) {
-                
-                int ronda =1;
+                seguentTorn = true;
+
                 if (torn >= noms.length) {
                     torn = 0;
                     ronda++;
+                    System.out.println("RONDA " + ronda + "\n");
                 }
                 System.out.println("Torn de: " + noms[torn] + ", està a la casella " + tauler[torn]);
                 int dice = 0;
-                int move;
-                move = tirarDau(dice, tauler, torn);
-                int dice1stRound=0;
-                if (ronda ==1){
-                    dice1stRound = move;
-                    
-                }
-                System.out.println(noms[torn] + " ha tret un " + move);
-                
-                tauler[torn] = tauler[torn] + move;
-                daus3_6_4_5(tauler, torn, dice1stRound);
-                ocaEnOca(tauler, torn);
-                comprovarCasella63(tauler, torn);
+                int dau1;
+                int dau2 = 0;
 
-                
+                if (sanció[torn]) {
+                    rondesSanció[torn]--;
+                    System.out.println(noms[torn] + " està sancionat i perd el torn.");
+                    if (rondesSanció[torn] == 0) {
+                        sanció[torn] = false;
+                    }
+                } else {
+                    if (tauler[torn] < 60) {
+                        dau1 = tirarDau(dice, tauler, torn);
+                        dau2 = tirarDau(dice, tauler, torn);
+                    } else {
+                        dau1 = tirarDau(dice, tauler, torn);
+                        dau2 = 0;
+                    }
+
+                    System.out.println(noms[torn] + " ha tret un " + dau1 + " i " + dau2 + ": " + (dau1 + dau2));
+                    int move = dau1 + dau2;
+                    tauler[torn] += move;
+                    if (ronda == 1) {
+
+                        daus3_6_4_5(tauler, torn, dau1, dau2);
+                    }
+
+                    pont(tauler, torn);
+                    ocaEnOca(tauler, torn);
+
+                    System.out.println(noms[torn] + " es mou fins la casella: " + tauler[torn]);
+                    comprovarCasella63(tauler, torn);
+                    
+
+                    fonda(tauler, torn, rondesSanció);
+                    pou(tauler, torn, rondesSanció);
+                }
+                System.out.println("");
             }
+
             torn++;
         } while (!end);
         System.out.println("Ha acabat el joc");
@@ -151,33 +180,37 @@ public class JocOca {
 
     public int tirarDau(int dice, int tauler[], int torn) {
         if (tauler[torn] < 60) {
-            dice = (int) (Math.random() * 12) + 1;
+            dice = (int) (Math.random() * 6) + 1;
         } else {
             dice = (int) (Math.random() * 6) + 1;
         }
         return dice;
     }
 
-public void daus3_6_4_5(int tauler[], int torn, int dice ){
-if (dice == 3 ||dice==6 ){
-    tauler[torn]= 26;
-    System.out.println("De dado a dado y tiro porque me ha tocado.");
-    seguentTorn = false;
-}
-if (dice == 4 ||dice==5 ){
-    tauler[torn]= 53;
-    System.out.println("De dado a dado y tiro porque me ha tocado.");
-    seguentTorn = false;
-}
-}
+    public void daus3_6_4_5(int tauler[], int torn, int d1, int d2) {
+        if (d2 == 3 && d1 == 6) {
+            tauler[torn] = 26;
+            System.out.println("De dado a dado y tiro porque me ha tocado.");
+            seguentTorn = false;
+        } else if (d1 == 6 && d2 == 3) {
+            tauler[torn] = 26;
+            System.out.println("De dado a dado y tiro porque me ha tocado.");
+            seguentTorn = false;
+        }
+        if (d1 == 4 && d2 == 5) {
+            tauler[torn] = 53;
+
+        } else if (d2 == 4 && d1 == 5) {
+            tauler[torn] = 53;
+
+        }
+    }
 
     public void comprovarCasella63(int tauler[], int torn) {
-        if (tauler[torn] < 63) {
-            System.out.println(noms[torn] + " es mou fins la casella: " + tauler[torn] + "\n");
-        } else if (tauler[torn] == 63) {
-            System.out.println(noms[torn] + " es mou fins la casella: " + tauler[torn]);
+        if (tauler[torn] == 63) {
+
             System.out.println("El jugador " + noms[torn] + " ha guanyat");
-            seguentTorn = false;
+            seguentTorn = true;
             end = true;
         } else if (tauler[torn] > 63) {
             int sobrants = 0;
@@ -201,9 +234,51 @@ if (dice == 4 ||dice==5 ){
                     tauler[torn] = oques[i + 1];
                     System.out.println("De oca a oca! " + noms[torn] +
                             " avança fins la casella " + tauler[torn]);
+                    seguentTorn = false;
                 }
                 return;
             }
+
+        }
+    }
+
+    public void pont(int tauler[], int torn) {
+        if (tauler[torn] == 6) {
+            System.out.println("Casella 6: De puente a puente y tiro porque me lleva la corriente.");
+            System.out.println("Has abançat fins la casella 12");
+
+            tauler[torn] = 12;
+            seguentTorn = false;
+        } else if (tauler[torn] == 12) {
+            System.out.println("Casella 12: De puente a puente y tiro porque me lleva la corriente.");
+            System.out.println("Has retrocedit fins la casella 6");
+
+            tauler[torn] = 6;
+            seguentTorn = false;
+        }
+    }
+
+    public void fonda(int tauler[], int torn, int[] rondes) {
+
+        if (tauler[torn] == 19) {
+            System.out.println("Has caigut a la fonda! Perds el pròxim torn.\n");
+            sanció[torn] = true;
+            rondes[torn] = 1;
+        }
+    }
+
+    public void pou(int tauler[], int torn, int[] rondes) {
+        if (tauler[torn] == 31) {
+            System.out.println("Has caigut a al Pou! Perds el pròxim torn.\n");
+            for (int i = 0; i < tauler.length; i++){
+                if (sanció[i] && tauler[i]==31){
+                    sanció[i] = false;
+                rondes[i] = 0;
+                System.out.println(noms[i] + " surt del pou.");
+                }
+            }
+            sanció[torn] = true;
+            rondes[torn] = 2;
 
         }
     }
